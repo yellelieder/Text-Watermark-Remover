eae71d6fd9e8243140c3066f72959dde95a62e6e
yellelieder <55919613+yellelieder@users.noreply.github.com>
2022-12-19 07:10:11 +0100

    clean up and test data

--- script.py
+++ script.py
@@ -7,19 +7,13 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-#https://towardsdatascience.com/remove-text-from-images-using-cv2-and-keras-ocr-24e7612ae4f4
-
-#--KMP_DUPLICATE_LIB_OK=TRUE
-
 def midpoint(x1, y1, x2, y2):
     x_mid = int((x1 + x2)/2)
     y_mid = int((y1 + y2)/2)
     return (x_mid, y_mid)
 
 def inpaint_text(img_path, pipeline):
-    # read image
     img = keras_ocr.tools.read(img_path)
-    # generate (word, box) tuples 
     prediction_groups = pipeline.recognize([img])
     mask = np.zeros(img.shape[:2], dtype="uint8")
     for box in prediction_groups[0]:
@@ -41,7 +35,8 @@ def inpaint_text(img_path, pipeline):
 
 if __name__ =="__main__":
     pipeline = keras_ocr.pipeline.Pipeline()
-    img=inpaint_text("n02085936_66.jpg", pipeline)
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    cv2.imwrite("textfree.jpg",img_rgb)
-    plt.imshow(img)
+    input_folder_path = 'input_folder'
+    for filename in os.listdir(input_folder_path):
+        img=inpaint_text(f"{input_folder_path}/{filename}", pipeline)
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        cv2.imwrite(f"clean_images/{filename}",img_rgb)
